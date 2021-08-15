2472618b946cac43ef74197a3a4a80a9a3728b4a
shaneyTEC <63270555+shaneyTEC@users.noreply.github.com>
2021-08-14 21:10:22 -0600

    Despliegue de la lista de los juegos

--- src/app/componentes/card-game/card-game.component.ts
+++ src/app/componentes/card-game/card-game.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from 'src/app/models/Game';
 import { GameService } from 'src/app/services/game.service';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
 
 @Component({
   selector: 'app-card-game',
@@ -10,10 +12,10 @@ import { GameService } from 'src/app/services/game.service';
 export class CardGameComponent implements OnInit {
 
   @Input()
-  game: Game = new Game; 
+  game: Game = new Game;
 
   // @Input()
-  // idGame: string | undefined; 
+  // idGame: string | undefined;
 
   constructor(private gameService: GameService) { }
 
@@ -21,8 +23,8 @@ export class CardGameComponent implements OnInit {
 
     // this.gameService.getGame('game-1').subscribe({
     //   next: res => {
-    //     this.game = res;       
-    //     console.log(this.game);       
+    //     this.game = res;
+    //     console.log(this.game);
     //   }, error: err => {
     //     console.log(err);
     //   }
