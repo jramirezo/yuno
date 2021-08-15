fc9a4563b6db6dbf6aaf2d35074956356760d3b8
Juan Diego <jarguelloq@ucenfotec.ac.cr>
2021-08-15 07:27:45 -0600

    url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true, 
- // API_URL: 'https://backend-yuno.azurewebsites.net/'
-  API_URL: 'http://localhost:8080/'
+  API_URL: 'https://backend-yuno.azurewebsites.net/'
+ // API_URL: 'http://localhost:8080/'
 };
